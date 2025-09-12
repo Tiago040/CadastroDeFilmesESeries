e67c3df6b0706b100d8222eb855f5e82271287e6
Tiago040 <tiago.lopes040@gmail.com>
2025-09-12 19:16:10 -0300

    first commit

---
+++ src/main/java/com/movies/informacoes_cinema/InformacoesCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.movies.informacoes_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformacoesCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformacoesCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movies/informacoes_cinema/InformacoesCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.informacoes_cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformacoesCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
